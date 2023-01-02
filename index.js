0027c189208590dd5000d4a4c9a053b85ee31d64
dac <dac@dypm.org>
2023-01-02 17:15:01 +0530

    changes

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express=require("express");
 const path=require("path");
 const app = express();
-const p = path.join(__dirname,)
-
 app.get("/",(req,res)=>{
-    res.send(path.join(__dirname , "./iacsd.html"));
+    res.sendFile(path.join(__dirname , "./public/iacsd.html"));
 });
 app.listen("3000",()=>{
     console.log("connection successful");
